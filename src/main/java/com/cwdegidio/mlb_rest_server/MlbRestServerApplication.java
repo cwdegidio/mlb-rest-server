ebbca15a65905f7d05491c1c21b8118b7f3ebd2a
Curtis W. DeGidio <curtis.degidio@gmail.com>
2024-06-12 00:18:50 -0500

    First commit.

---
+++ src/main/java/com/cwdegidio/mlb_rest_server/MlbRestServerApplication.java
@@ -0,0 +1,13 @@
+package com.cwdegidio.mlb_rest_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlbRestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlbRestServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cwdegidio/mlb_rest_server/MlbRestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwdegidio.mlb_rest_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlbRestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
